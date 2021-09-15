f720d448902f6d5cfeedff114174324a089ca53b
Yeferson <yefersonvelandia4@gmail.com>
2021-09-14 19:24:17 -0500

    Proyecto peticiones http

---
+++ README.md
@@ -0,0 +1,16 @@
+# Peticiones htttp - CRUD
+
+consumo de la api https://reqres.in/
+
+## Nota
+Recuerden que para reconstruir los módulos de node deben de ejecutar este comando
+
+```
+npm install
+```
+
+Y para ejecutar
+
+```
+npm start
+```
---
+++ src/index.js
@@ -0,0 +1,50 @@
+//peticiones http
+
+import * as CRUD from './js/crud-provider';
+
+// CRUD.getUsuario(2).then(console.log);
+
+// CRUD.crearUsuario({
+//     name: 'Carlos',
+//     job: 'Carpintero'
+// }).then( console.log );
+
+// CRUD.actualizarUsuario( 1, {
+//     name: 'melissa',
+//     job:  'developer'
+// }).then(console.log);
+
+CRUD.eliminarUsuario( 1 ).then( console.log );
+
+
+
+// import { init } from "./js/usuarios-page";
+// init();
+
+// import { obtenerUsuarios } from "./js/http-provider";
+
+// obtenerUsuarios().then(console.log);
+
+
+
+
+
+
+// import { init } from "./js/page-chiste";
+
+// init()
+// const url = 'https://api.chucknorris.io/jokes/random';
+
+// // fetch(url).then( resp => {
+
+// // 	resp.json().then({id, value} => {
+// // 		console.log(id);
+// // 		console.log(value);
+// // 	});
+
+// // });
+
+// fetch(url).then( resp => resp.json() )
+// 	.then( ({id, value }) =>{
+// 			console.log(id, value);
+// 		});
